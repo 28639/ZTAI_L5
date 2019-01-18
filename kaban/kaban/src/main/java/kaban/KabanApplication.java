1b1ceb2e1141137a154a06ff0dbd1dd39aeef702
Michał Matracz <mathracz@gmail.com>
2019-01-18 14:13:00 +0100

    dodanie springa

---
+++ kaban/kaban/src/main/java/kaban/KabanApplication.java
@@ -0,0 +1,14 @@
+package kaban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabanApplication.class, args);
+	}
+
+}
+
---
+++ kaban/kaban/src/test/java/kaban/kaban/KabanApplicationTests.java
@@ -0,0 +1,17 @@
+package kaban.kaban;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KabanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
